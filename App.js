c510bdc0e0fb9f734ec7a73297ba081f6d52a77a
emir ari <emirariomatic@gmail.com>
2024-03-14 12:39:30 +0300

    First break for breakfast

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, Image } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import colors from './assets/colors/colors';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      {/* TOP BAR VIEW STARTS HERE ! */}
+      <SafeAreaView style={styles.safeArea}>
+        <View style={styles.topBarView}>
+          <View style={styles.topBarLeft}>
+            <Icon name='menu' size={24} color={'#212121'}></Icon>
+          </View>
+          <View style={styles.topBarLogo}>
+            <Text>BiLira</Text>
+          </View>
+          <View style={styles.topBarRight}>
+            <Icon name='search' size={24} color={'#212121'}></Icon>
+            <Icon name='bell' size={24} color={'#212121'}></Icon>
+          </View>
+        </View>
+      </SafeAreaView>
+
+      {/* BUDGET VIEW STARTS HERE ! */}
+      <View style={styles.budgetView}></View>
+      <View style={styles.buttonsView}></View>
+      <View style={styles.inviteFriendView}></View>
+      <View style={styles.marketsView}></View>
+      <View style={styles.coinsView}></View>
     </View>
   );
 }
@@ -13,8 +36,66 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+
+  safeArea: {
+    flex: 1,
+  },
+
+  // 1 --
+  topBarView: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 10,
+    justifyContent: 'flex-start',
+    backgroundColor: colors.green,
+  },
+
+  topBarLeft: {
+    flex: 1,
+  },
+
+  topBarLogo: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-start'
+  },
+
+  topBarRight: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-end'
+  },
+
+  // 2 --
+  budgetView: {
+    flex: 1,
+    backgroundColor: colors.orange,
+  },
+
+  // 3 --
+  buttonsView: {
+    flex: 1,
+    backgroundColor: colors.purple,
+  },
+
+  // 4 --
+  inviteFriendView: {
+    flex: 1,
+    backgroundColor: colors.red,
+  },
+
+  // 5 --
+  marketsView: {
+    flex: 1,
+    backgroundColor: colors.blue,
+  },
+
+  // 6 --
+  coinsView: {
+    flex: 1,
+    backgroundColor: colors.lightGreen,
+  },
+
 });
---
+++ assets/colors/colors.js
@@ -0,0 +1,10 @@
+const colors = {
+    purple: '#7900FF',
+    blue: '#548CFF',
+    green: '#93FFD8',
+    lightGreen: '#CFFFDC',
+    red: '#D04848',
+    orange: '#F3B95F',
+}
+
+export default colors;
