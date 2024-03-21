903f2f3c3bd9ca1fbc6e91ecda33de1e319da1d9
emir ari <emirariomatic@gmail.com>
2024-03-21 17:35:05 +0300

    Custom button improvement

--- assets/components/custom-button.js
+++ assets/components/custom-button.js
@@ -1,17 +1,24 @@
 import { Dimensions, StyleSheet, View, Text } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import colors from '../../assets/colors/colors.js';
-import { TouchableHighlight } from 'react-native-gesture-handler';
 import { TouchableOpacity } from '@gorhom/bottom-sheet';
+import { useState } from 'react';
 
 const screenWidth = Dimensions.get('window').width;
 const screenHeight = Dimensions.get('window').height;
 const buttonHeightRatio = 15.7777777778;
 const textSizeRatio = 65.5384615385;
 
-const CustomButton = ( {onPress, title, isIconLeft, iconLeft, isIconRight, iconRight, buttonColor} ) => {
+const CustomButton = ( { title, isIconLeft, iconLeft, isIconRight, iconRight, buttonColor} ) => {
 
-    
+    const [isDisabled, setIsDisabled] = useState(false);
+
+    const handlePress = () => {
+        if (!isDisabled) {
+            setIsDisabled(true),
+            setTimeout(() => {setIsDisabled(false)}, 2000);
+        }
+    }
 
     const customButtonStyles = StyleSheet.create({
         container: {
@@ -53,15 +60,25 @@ const CustomButton = ( {onPress, title, isIconLeft, iconLeft, isIconRight, iconR
 
     return (
         <View style={customButtonStyles.container}>
-            <TouchableOpacity onPress={onPress}>
-                <View style={customButtonStyles.buttonBackground}>
+            {isDisabled ? (
+                <View style={{backgroundColor: colors.primaryBlue100, borderRadius: 64}}>
                     <View style={customButtonStyles.buttonContent}>
                         <Icon style={isIconLeft === 'true' ? customButtonStyles.iconLeft : customButtonStyles.blank} name={isIconLeft === 'true' ? iconLeft : ''} size={18} color={colors.primaryWhite}/>
-                        <Text style={typeof title !== 'string' ? customButtonStyles.blank : customButtonStyles.titleStyle}>{title}</Text>
+                        <Text style={typeof title !== 'string' ? customButtonStyles.blank : customButtonStyles.titleStyle}>{typeof title !== 'string' ? null : 'Executing'}</Text>
                         <Icon style={isIconRight === 'true' ? customButtonStyles.iconRight : customButtonStyles.blank} name={isIconRight === 'true' ? iconRight : ''} size={18} color={colors.primaryWhite}/>
                     </View>
                 </View>
-            </TouchableOpacity>
+            ) : (
+                <TouchableOpacity onPress={handlePress} activeOpacity={1.0}>
+                    <View style={customButtonStyles.buttonBackground}>
+                        <View style={customButtonStyles.buttonContent}>
+                            <Icon style={isIconLeft === 'true' ? customButtonStyles.iconLeft : customButtonStyles.blank} name={isIconLeft === 'true' ? iconLeft : ''} size={18} color={colors.primaryWhite}/>
+                            <Text style={typeof title !== 'string' ? customButtonStyles.blank : customButtonStyles.titleStyle}>{title}</Text>
+                            <Icon style={isIconRight === 'true' ? customButtonStyles.iconRight : customButtonStyles.blank} name={isIconRight === 'true' ? iconRight : ''} size={18} color={colors.primaryWhite}/>
+                        </View>
+                    </View>
+                </TouchableOpacity>
+            )}
         </View>
     );
 }
